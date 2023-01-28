7e79a73fe41676e3c07096adf83baa0b33e138e6
Juan Pablo <juanp1310@outlook.com>
2023-01-28 12:54:29 -0300

    Custon dio e interceptor

--- lib/repositories/back4app/back4app_tarefas.dart
+++ lib/repositories/back4app/back4app_tarefas.dart
@@ -1,28 +1,22 @@
 import 'package:dio/dio.dart';
 import 'package:trilhaapp/model/hive/tarefas_back4app_model.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:trilhaapp/repositories/back4app/back4app_custon_dio.dart';
 
 class TarefasBack4AppRepository {
-  final _dio = Dio();
+  final _custonDio = Back4AppCustonDio();
 
-  TarefasBack4AppRepository() {
-    _dio.options.headers["X-Parse-Application-Id"] =
-        dotenv.get("XPARSEAPPLICATIONID");
-    _dio.options.headers["X-Parse-REST-API-Key"] =
-        dotenv.get("XPARSERESTAPIKEY");
-    _dio.options.headers["Content-Type"] = dotenv.get("CONTENTTYPE");
-    _dio.options.baseUrl = 'https://parseapi.back4app.com/classes/Tarefas';
-  }
+  TarefasBack4AppRepository();
 
   Future<TarefasBack4AppModel> obterTarefas(bool naoConcluidos) async {
     var query = naoConcluidos ? "?where={\"concluido\":true}" : "";
-    var result = await _dio.get(query);
+    var result = await _custonDio.dio.get("/Tarefas$query");
     return TarefasBack4AppModel.fromJson(result.data);
   }
 
   Future<void> criarTarefa(Tarefa tarefa) async {
     try {
-      var response = await _dio.post("", data: tarefa.toCreateJson());
+      var response = await _custonDio.dio.post("/Tarefas", data: tarefa.toCreateJson());
     } catch (e) {
       throw e;
     }
@@ -30,14 +24,14 @@ class TarefasBack4AppRepository {
 
   Future<void> atualizarTarefa(Tarefa tarefa) async {
     try {
-      var response = await _dio.put("/${tarefa.objectId}", data: tarefa.toJsonEndpoint());
+      var response = await _custonDio.dio.put("/Tarefas/${tarefa.objectId}", data: tarefa.toJsonEndpoint());
     } catch (e) {
       throw e;
     }
   }
   Future<void> deletarTarefa(Tarefa tarefa) async {
     try {
-      var response = await _dio.delete("/${tarefa.objectId}");
+      var response = await _custonDio.dio.delete("/Tarefas/${tarefa.objectId}");
     } catch (e) {
       throw e;
     }
